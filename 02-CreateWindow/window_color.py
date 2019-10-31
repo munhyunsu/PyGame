a9ef128c6d7c59e6fa31fa88884c3d94a36220c5
munhyunsu <munhyunsu@gmail.com>
2019-10-31 12:43:28 +0900

    base color

--- 02-CreateWindow/window_color.py
+++ 02-CreateWindow/window_color.py
@@ -10,6 +10,9 @@ SURFACE = pygame.display.set_mode((400, 300)) # PyGame Window Size (x, y)
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 
 def main():
